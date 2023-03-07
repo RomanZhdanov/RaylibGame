2ce49eac225227895dacb17feaef72af2c325b28
Роман Жданов <romanizh@ulh.ru>
2023-03-07 09:25:59 +0300

    Added attacj animations to player animations manager.

--- PotatoEngine/Player.cpp
+++ PotatoEngine/Player.cpp
@@ -18,6 +18,10 @@ Player::Player()
 	animations.add("run_up", new Animation(player, 6, 11, animationSpeed, true));
 	animations.add("run_left", new Animation(player, 12, 17, animationSpeed, true));
 	animations.add("run_down", new Animation(player, 18, 23, animationSpeed, true));
+	animations.add("attack_right", new Animation(player, 24, 27, animationSpeed, true));
+	animations.add("attack_up", new Animation(player, 28, 31, animationSpeed, true));
+	animations.add("attack_left", new Animation(player, 32, 35, animationSpeed, true));
+	animations.add("attack_down", new Animation(player, 36, 39, animationSpeed, true));
 }
 
 void Player::update(float delta)
